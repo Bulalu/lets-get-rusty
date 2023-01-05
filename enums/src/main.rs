bef31a4b03259169677f52329e1d7cc8c0d7d2db
Bulalu <elishabulalu031@gmail.com>
2023-01-05 22:24:49 +0300

    adios enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -15,6 +15,37 @@ struct matchResult {
     team: String
 }
 
+#[derive(Debug)]
+enum PlayerPosition {
+    Striker,
+    Winger
+}
+enum Players {
+    Ronaldo,
+    Mbappe,
+    Rashford(PlayerPosition),
+    Benzema
+}
+
+
+fn player_ratings(players: Players) -> u8 {
+    match players {
+        Players::Benzema => 90,
+        Players::Mbappe => 91,
+        Players::Rashford(state)=> {
+            println!("Player Position is {:?}!", state);
+            88
+        },
+        Players::Ronaldo => 89
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1)
+    }
+}
 fn main() {
 
     let mutd = matchResult {
@@ -33,5 +64,17 @@ fn main() {
     let some_number = Some(5);
     let some_char = Some("elisha");
     let absent_no: Option<i32> = None;
+
+    // let results = player_ratings(Players::Rashford(PlayerPosition::Striker));
+    // println!("{}", results)
+
+    let five = Some(5);
+    let six =  plus_one(five);
+    let none = plus_one(None);
+
+    println!("{:?}", six);
+    println!("{:?}", none);
+
+
     
 }

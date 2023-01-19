e0b7d56b94eadb316e75f9251b24e4e2bf2ca009
Bulalu <elishabulalu031@gmail.com>
2023-01-19 12:40:43 +0300

    friendly err message

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,5 +1,6 @@
 use  std::env;
-use std::fs;
+use  std::fs;
+use  std::process;
 
 
 
@@ -7,7 +8,12 @@ fn main() {
     let args:Vec<String> = env::args().collect();
 
     // let config = parse_config(& args);
-    let config = Config::new(&args);
+    let config = Config::build(&args).unwrap_or_else(
+        |err| {
+            println!("Problem parsing arguments: {err}");
+            process::exit(1);
+        }
+    );
 
     println!("Searching for {}", config.query);
     println!("In File {}", config.file_path);
@@ -24,11 +30,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Config { query: query, file_path: file_path }
+        Ok(Config { query: query, file_path: file_path })
     }
 }
 

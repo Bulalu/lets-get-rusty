749ea47da7a7dd23fb3058676e4a56f36ccf35f4
Bulalu <elishabulalu031@gmail.com>
2023-01-20 14:02:44 +0300

    added search functionality

---
+++ minigrep/target/debug/deps/minigrep-41ad9e75897d38a7.d
@@ -0,0 +1,5 @@
+/Users/elishabulalu/Documents/rust🦀/minigrep/target/debug/deps/minigrep-41ad9e75897d38a7: src/lib.rs
+
+/Users/elishabulalu/Documents/rust🦀/minigrep/target/debug/deps/minigrep-41ad9e75897d38a7.d: src/lib.rs
+
+src/lib.rs:
---
+++ minigrep/target/debug/deps/minigrep-bfa8b02c107836cb.d
@@ -0,0 +1,5 @@
+/Users/elishabulalu/Documents/rust🦀/minigrep/target/debug/deps/minigrep-bfa8b02c107836cb: src/main.rs
+
+/Users/elishabulalu/Documents/rust🦀/minigrep/target/debug/deps/minigrep-bfa8b02c107836cb.d: src/main.rs
+
+src/main.rs:
